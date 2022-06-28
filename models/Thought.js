4d81a858ba60c093b9e934eb90f66912b23f4b60
crumwj22 <crumwj22@hotmail.com>
2022-06-27 21:59:54 -0700

    folder layout, created connection.js, index.js, readme, and started the models

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create User model
+const thoughtSchema = new Schema(
+  {
+    first: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    last: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    github: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    assignments: [assignmentSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
